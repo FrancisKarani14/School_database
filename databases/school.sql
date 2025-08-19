8fc42a0573be4cb007789eb9869312b928ce8d5e
Francis <franciskarani15@gmail.com>
2025-08-19 14:52:18 +0300

    creates a teachers and students table

--- databases/school.sql
+++ databases/school.sql
@@ -1,4 +1,17 @@
+
+-- main table for teachers it holds the relationship
 CREATE TABLE teachers(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-name
+name TEXT NOT NULL,
+email TEXT UNIQUE,
+class TEXT NOT NULL
+);
+
+--  child table they are supposed to be many of them
+
+CREATE TABLE students(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT NOT NULL,
+email TEXT UNIQUE,
+student_adm INTEGER UNIQUE
 );
